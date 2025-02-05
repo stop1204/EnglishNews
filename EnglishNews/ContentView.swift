e3de637b0fa5a3ccc12d52e7478668cd67b97710
Terry <hl928452957@gmail.com>
2025-02-05 16:35:37 +0800

    Initial Commit

---
+++ EnglishNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EnglishNews
+//
+//  Created by HE HUALIANG on 05/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EnglishNews/EnglishNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishNewsApp.swift
+//  EnglishNews
+//
+//  Created by HE HUALIANG on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct EnglishNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
